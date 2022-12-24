6b66e50b5c786b79a65ce3df8272c4b15d444125
Anurag dhurwey <anuragdhurwey9211@gmail.co>
2022-12-24 21:12:41 +0530

    Api is added in this commit

--- src/index.js
+++ src/index.js
@@ -6,15 +6,26 @@ const requests=require("requests");
 
 
 const staticPath=path.join(__dirname,"../public");
-
 app.set("view engine","hbs")
-
 app.use(express.static(staticPath))
 app.set('views','views/')
 
 
 app.get("/",(req,res)=>{
-  res.render("index.hbs")
+  
+  requests('https://api.openweathermap.org/data/2.5/weather?q=bhopal&appid=099029477bb8bd68ccf37540ba19ea39', )
+  .on('data',  (chunk)=> {
+      let objdata=JSON.parse(chunk);
+      const arr_data=[objdata];  
+      res.render("index.hbs",{
+        Location:   arr_data[0].name,
+        Country:   arr_data[0].sys.country,
+        Temprature:   Math.ceil(arr_data[0].main.temp-273.15),
+        Min_Temp:   Math.ceil(arr_data[0].main.temp_min-273.15),
+        Max_Temp:   Math.ceil(arr_data[0].main.temp_max-273.15)})  
+  });
+     
+  
 })
 
 app.listen(port, () => {
